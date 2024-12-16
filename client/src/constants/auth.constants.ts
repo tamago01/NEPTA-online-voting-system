d64f26494af1c0c2ea2571fa269c217a9e17a9d1
Pradeep Lama <tamangjake@gmail.com>
2024-12-17 02:25:58 +0545

    deploy fix

--- client/src/constants/auth.constants.ts
+++
@@ -1,3 +0,0 @@
-export const EXPIRES_IN = Date.now() + 1 * 60 * 60 * 1000; // 1 hr in milliseconds
-export const REFRESH_EXPIRES_IN = Date.now() + 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
-export const REFRESH_INTERVAL = 10 * 60 * 1000; // 20 minutes
