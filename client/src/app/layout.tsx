3c86ba3a05d7ac18bc6e658b861782277f22f782
Pradeep Lama <tamangjake@gmail.com>
2024-12-17 12:14:04 +0545

    deploy fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,69 +1,7 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import "./globals.css";
 
-import { createContext, useContext } from "react";
-
-interface TimerContextType {
-  isTimerActive: boolean;
-  timerValue: number;
-  startTimer: () => void;
-  showBackdrop: boolean;
-}
-
-const TimerContext = createContext<TimerContextType>({
-  isTimerActive: false,
-  timerValue: 10,
-  startTimer: () => {},
-  showBackdrop: true,
-});
-
-export function TimerProvider({ children }: { children: React.ReactNode }) {
-  const [isTimerActive, setIsTimerActive] = useState(false);
-  const [timerValue, setTimerValue] = useState(10);
-  const [showBackdrop, setShowBackdrop] = useState(true);
-
-  const startTimer = () => {
-    setIsTimerActive(true);
-    setShowBackdrop(true);
-  };
-
-  useEffect(() => {
-    let timerInterval: NodeJS.Timeout;
-
-    if (isTimerActive) {
-      timerInterval = setInterval(() => {
-        setTimerValue((prevValue) => {
-          const newValue = prevValue > 0 ? prevValue - 1 : 0;
-          if (newValue === 0) {
-            clearInterval(timerInterval);
-            setIsTimerActive(false);
-            setShowBackdrop(false);
-          }
-
-          return newValue;
-        });
-      }, 1000);
-    }
-    return () => {
-      if (timerInterval) {
-        clearInterval(timerInterval);
-      }
-    };
-  }, [isTimerActive]);
-
-  return (
-    <TimerContext.Provider
-      value={{ isTimerActive, timerValue, startTimer, showBackdrop }}
-    >
-      {children}
-    </TimerContext.Provider>
-  );
-}
-
-export function useTimer() {
-  return useContext(TimerContext);
-}
+import React from "react";
+import { TimerProvider } from "./context/TimeProvider";
 
 export default function RootLayout({
   children,
@@ -72,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-slate-100  relative`}>
+      <body className="bg-slate-100 relative">
         <TimerProvider>{children}</TimerProvider>
       </body>
     </html>
