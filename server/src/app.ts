c63f0e4d578c0f5be72c24b27aa77a0ce425b470
Aashutosh <aashutosh.poudel8@gmail.com>
2024-12-17 14:57:03 +0545

    added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,7 @@ export class App {
     this.app.use(express.json());
     this.app.use(
       cors({
-        origin: "http://localhost:3000 || https://nepta-client.vercel.app",
+        origin: "*",
         credentials: true,
       })
     );
