0449e0503be298e340170307426b1c831c19c53b
Aashutosh <aashutosh.poudel8@gmail.com>
2024-12-19 15:34:27 +0545

    proxy test

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  // experimental: {
-  //   serverActions: {
-  //     allowedOrigins: ["https://nepta-online-voting-system.onrender.com",
-  //       "https://nepta.onrender.com","*"
-  //     ],
-  //   },
-  // },
   trailingSlash: false,
   images: {
     domains: [
